3f5232a4c7c508c99c4b48ba359b8e7dceced242
Jordan Walker <jordan@jayy.online>
2016-08-25 13:24:14 +0100

    Added composer, htop and thefuck to the .config

---
+++ .config/thefuck/settings.py
@@ -0,0 +1,20 @@
+# The Fuck settings file
+#
+# The rules are defined as in the example bellow:
+#
+# rules = ['cd_parent', 'git_push', 'python_command', 'sudo']
+#
+# The default values are as follows. Uncomment and change to fit your needs.
+# See https://github.com/nvbn/thefuck#settings for more information.
+#
+
+# rules = [<const: All rules enabled>]
+# priority = {}
+# alter_history = True
+# require_confirmation = True
+# wait_command = 3
+# debug = False
+# exclude_rules = []
+# env = {'LC_ALL': 'C', 'LANG': 'C', 'GIT_TRACE': '1'}
+# history_limit = None
+# no_colors = False

143e6c62c3271d7dc6bbe988026b5373da929a17
Jordan Walker <jordan@jayy.online>
2016-09-14 12:01:35 +0100

    Fixed directory mistake in install script

--- .scripts/install.sh
+++ .scripts/install.sh
@@ -6,7 +6,7 @@ ln -s /home/$USER/.dotfiles/.config/ /home/$USER/.config/
 
 ln -s /home/$USER/.dotfiles/.ctags /home/$USER/.ctags
 
-ln -s /home/$USER/.dotfiles/zsh/.oh-my-zsh/ home/$USER/.oh-my-zsh
+ln -s /home/$USER/.dotfiles/zsh/.oh-my-zsh/ /home/$USER/.oh-my-zsh
 
 ln -s /home/$USER/.dotfiles/.tmux.conf /home/$USER/.tmux.conf
 

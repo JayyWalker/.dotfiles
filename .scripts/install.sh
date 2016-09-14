5a31c13a4a72774b4d9723672d8fb24e6966f392
Jordan Walker <jordan@jayy.online>
2016-09-14 12:01:06 +0100

    Setup file

---
+++ .scripts/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+ln -s /home/$USER/.dotfiles/.agignore /home/$USER/.dotfiles/.agignore
+
+ln -s /home/$USER/.dotfiles/.config/ /home/$USER/.config/
+
+ln -s /home/$USER/.dotfiles/.ctags /home/$USER/.ctags
+
+ln -s /home/$USER/.dotfiles/zsh/.oh-my-zsh/ home/$USER/.oh-my-zsh
+
+ln -s /home/$USER/.dotfiles/.tmux.conf /home/$USER/.tmux.conf
+
+ln -s /home/$USER/.dotfiles/vim/.vim /home/$USER/.vim
+
+ln -s /home/$USER/.dotfiles/vim/.vimrc /home/$USER/.vimrc
+
+ln -s /home/$USER/.dotfiles/zsh/.zshrc /home/$USER/.zshrc
+
+sudo ln -s /home/$USER/.dotfiles/.scripts/vhosts /usr/local/bin/vhosts

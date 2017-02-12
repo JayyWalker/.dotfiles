2803012a08d15cbb4052959b9e16b0a5a426f8a4
Jordan Walker <jordan@jayy.online>
2017-02-12 22:42:54 +0000

    Fixed fix-zsh script

--- .scripts/fix-zsh.sh
+++ .scripts/fix-zsh.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-mv ~/.zsh_history ~/.zsh_history_bad && strings ~/.zsh_history_bad > ~/.zsh_history && fc -R ~/.zsh_history
+cd ~
+
+mv .zsh_history .zsh_history_bad 
+
+strings .zsh_history_bad > .zsh_history 
+
+fc -R .zsh_history
+
+cd -

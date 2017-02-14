816bf82ecf05901a7f35e5161dadf18e017af935
JayyWalker <jordan@jayy.online>
2017-02-14 12:19:24 +0000

    Cleaned up the install script

--- .scripts/install.sh
+++ .scripts/install.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
+
+TEXTCOLOR='\033[1;34m'
+
+printf "${TEXTCOLOR}Copied .agignore to $HOME/.agignore\n"
+ln -s $HOME/.dotfiles/.agignore $HOME/.agignore
+
+printf "${TEXTCOLOR}Copied .config $HOME/.config\n"
+ln -s $HOME/.dotfiles/.config $HOME/.config
+
+printf "${TEXTCOLOR}Copied .ctags $HOME/.ctags\n"
+ln -s $HOME/.dotfiles/.ctags $HOME/.ctags
+
+printf "${TEXTCOLOR}Copied .oh-my-zsh $HOME/.oh-my-zsh\n"
+ln -s $HOME/.dotfiles/zsh/.oh-my-zsh $HOME/.oh-my-zsh
+
+printf "${TEXTCOLOR}Copied .tmux.conf $HOME/.tmux.conf\n"
+ln -s $HOME/.dotfiles/.tmux.conf $HOME/.tmux.conf
+
+printf "${TEXTCOLOR}Copied .vim $HOME/.vim\n"
+ln -s $HOME/.dotfiles/vim/.vim $HOME/.vim
+
+printf "${TEXTCOLOR}Copied .vimrc $HOME/.vimrc\n"
+ln -s $HOME/.dotfiles/vim/.vimrc $HOME/.vimrc
+
+printf "${TEXTCOLOR}Copied .zshrc $HOME/.zshrc\n"
+ln -s $HOME/.dotfiles/zsh/.zshrc $HOME/.zshrc
+
+printf "${TEXTCOLOR}Copied vhosts script to /usr/local/bin/vhosts\n"
+sudo ln -s $HOME/.dotfiles/.scripts/vhosts /usr/local/bin/vhosts
+
+printf "${TEXTCOLOR}Cloning lukechilds/zsh-nvm to ~/.oh-my-zsh/custom/plugins/zsh-nvm\n"
 git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
 
-ln -s /home/$USER/.dotfiles/.agignore /home/$USER/.agignore
-
-ln -s /home/$USER/.dotfiles/.config /home/$USER/.config
-
-ln -s /home/$USER/.dotfiles/.ctags /home/$USER/.ctags
-
-ln -s /home/$USER/.dotfiles/zsh/.oh-my-zsh /home/$USER/.oh-my-zsh
-
-ln -s /home/$USER/.dotfiles/.tmux.conf /home/$USER/.tmux.conf
-
-ln -s /home/$USER/.dotfiles/vim/.vim /home/$USER/.vim
-
-ln -s /home/$USER/.dotfiles/vim/.vimrc /home/$USER/.vimrc
-
-ln -s /home/$USER/.dotfiles/zsh/.zshrc /home/$USER/.zshrc
-
-sudo ln -s /home/$USER/.dotfiles/.scripts/vhosts /usr/local/bin/vhosts

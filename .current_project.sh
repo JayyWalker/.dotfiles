77f30084515ff8d3ec540e1e7caee3302b4c6555
Jordan Walker <jordan@jayy.online>
2017-11-13 13:38:59 +0000

    Terminal messed up. Removed pretty much everything

--- .current_project.sh
+++
@@ -1,6 +0,0 @@
-export CUR_PRJ=
-
-# PRJ_ENV is the framework being used. wp = wordpress; drupal, etc. 
-export PRJ_ENV=
-export THEME_NAME=
-export THEME_PATH=
--- .dbconfig.sh
+++
@@ -1,2 +0,0 @@
-export DBU=
-export DBP=
